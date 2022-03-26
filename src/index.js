296a91cb845421c09a1a6a9684ebf241598aa4ec
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 14:45:45 +0430

    Import mdb

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'mdb-ui-kit/css/mdb.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
